f952daeef5b3b6eb65ba844973ff61126ad48820
Oliver Witzki <Oliver.Witzki@jn.de>
2016-07-18 09:31:17 +0200

    Slidebox autoPlay property as one-way-binding

--- release/js/ionic-angular.js
+++ release/js/ionic-angular.js
@@ -4247,6 +4247,11 @@ IonicModule
    * explicitly told to do so.
    */
   'stop',
+  /**
+   * @ngdoc method
+   * @name $ionicSlideBoxDelegate#start
+   * @description Start sliding again if the slideBox was stopped.
+   */
   'autoPlay',
   /**
    * @ngdoc method
@@ -13102,7 +13107,7 @@ function($animate, $timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $i
     replace: true,
     transclude: true,
     scope: {
-      autoPlay: '=',
+      autoPlay: '@',
       slideDuration: '@',
       doesContinue: '@',
       slideInterval: '@',
